bf2578126cffa70310adafea21aa5f9e9797b213
adammajakil <4334841@students.swinburne.edu.my>
2017-03-17 13:57:52 +0800

    add font and modified the scoreboard

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -54,19 +54,21 @@ namespace CardGames
 		private static void DrawGame(Snap myGame)
 		{
 			SwinGame.DrawBitmap("cardsBoard.png", 0, 0);
+			SwinGame.LoadFontNamed ("GameFont", "Chunkfive.otf", 24);       //set the font type to Chunkfive
 
 			// Draw the top card
 			Card top = myGame.TopCard;
 			if (top != null)
 			{
-				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
-				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
-				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
+				
+				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.White, "GameFont", 0, 0);
+				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.White, "GameFont", 0, 35);
+				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.White, "GameFont", 0, 70);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 521, 153);
 			}
 			else
 			{
-				SwinGame.DrawText ("No card played yet...", Color.RoyalBlue, 0, 20);
+				SwinGame.DrawText ("No card played yet...", Color.White, "GameFont", 0, 30);
 			}
 
 			// Draw the back of the cards... to represent the deck
